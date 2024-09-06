f28190d2de9026b14904bedef32bc2028f4132c4
Dmytro Mykhailenko <mismartconfig@gmail.com>
2024-09-06 13:51:39 +0300

    update start script for users without sudo

--- start.sh
+++ start.sh
@@ -4,45 +4,58 @@ GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
 WHITE="\033[0m"
 
-export DOMAIN=${1}
-export PWDIR=$(pwd)
+DOMAIN=${1}
+PWDIR=$(pwd)
 
 if [ "${DOMAIN}" == "" ]; then
   echo -e "\n${RED}Error!${WHITE} Domain not specified"
-else
-    nginx_conf="0-certbot-${DOMAIN}.conf"
-    echo -e "server {\n\
-        listen 80;\n\
-        listen [::]:80;\n\
-    \n\
-        server_name $DOMAIN;\n\
-    \n\
-        location ~ /.well-known/acme-challenge {\n\
-            allow all;\n\
-            root ${PWDIR}/project/;\n\
-        }\n\
-    \n\
-    }" > ${nginx_conf}
-    sudo mv ${nginx_conf} /etc/nginx/conf.d/
-    sudo nginx -s reload
-
-    mkdir -p project ${DOMAIN}
-    docker run \
-      --name certbot-${DOMAIN} \
-      -v ${PWDIR}/project:${PWDIR}/project \
-      certbot/certbot \
-      certonly --webroot --webroot-path=${PWDIR}/project --email mail@gmail.com --agree-tos --no-eff-email -d ${DOMAIN}
-
-    if [ $? -eq 0 ]; then
-        echo -e "\n${GREEN}Successfully${WHITE} creating ssl keys"
-        docker cp certbot-${DOMAIN}:/etc/letsencrypt/archive/${DOMAIN}/fullchain1.pem ${DOMAIN}/fullchain.pem
-        docker cp certbot-${DOMAIN}:/etc/letsencrypt/archive/${DOMAIN}/privkey1.pem ${DOMAIN}/privkey.pem
-    else
-        echo -e "\n${RED}Fail${WHITE} creating ssl keys"
-        docker cp certbot-${DOMAIN}:/var/log/letsencrypt/letsencrypt.log ./letsencrypt.log
-    fi
-
-    docker rm -f certbot-${DOMAIN}
-    sudo rm /etc/nginx/conf.d/${nginx_conf}
-    sudo nginx -s reload
+  exit 2
 fi
+if [ "$(touch /etc/nginx/conf.d/- && rm /etc/nginx/conf.d/- ; echo $?)" == "1" ]; then
+	echo -e "\n${RED}Error!${WHITE} User not access write file to /etc/nginx/conf.d/ dir.\nRun and relogin '${USER}' user to host:\nsudo usermod -aG www-data ${USER}\nsudo chown root:www-data /etc/nginx/conf.d\nsudo chmod g+rw /etc/nginx/conf.d\n"
+	exit 2
+fi
+if [ "$(docker ps 1> /dev/null ; echo $?)" == "1" ]; then
+	echo -e "\n${RED}Error!${WHITE} User not access to execute docker commands.\nRun and relogin '${USER}' user to host:\nsudo usermod -aG docker ${USER}\n"
+	exit 2
+fi
+
+if [ "$(sudo nginx -s reload 1> /dev/null ; echo $?)" == "1" ]; then
+    echo -e "\n${RED}Error${WHITE} when execute nginx commands.\nRun and relogin '${USER}' user to host:\nsudo usermod -aG www-data ${USER}\nsudo visudo  # and paste at the end: %www-data ALL=(root) NOPASSWD: /usr/sbin/nginx -s reload"
+	exit 2
+fi
+
+CERTBOT_NGINX_CONF="
+server {
+    listen 80;
+    listen [::]:80;
+
+    server_name ${DOMAIN};
+
+    location ~ /.well-known/acme-challenge {
+        allow all;
+        root ${PWDIR}/project/;
+    }
+}"
+echo -e "${CERTBOT_NGINX_CONF}" > /etc/nginx/conf.d/0-certbot-${DOMAIN}.conf
+sudo nginx -s reload
+
+mkdir -p project ${DOMAIN}
+docker run \
+  --name certbot-${DOMAIN} \
+  -v ${PWDIR}/project:${PWDIR}/project \
+  certbot/certbot \
+  certonly --webroot --webroot-path=${PWDIR}/project --email mail@gmail.com --agree-tos --no-eff-email -d ${DOMAIN}
+
+if [ $? -eq 0 ]; then
+    echo -e "\n${GREEN}Successfully${WHITE} creating ssl keys"
+    docker cp certbot-${DOMAIN}:/etc/letsencrypt/archive/${DOMAIN}/fullchain1.pem ${DOMAIN}/fullchain.pem
+    docker cp certbot-${DOMAIN}:/etc/letsencrypt/archive/${DOMAIN}/privkey1.pem ${DOMAIN}/privkey.pem
+else
+    echo -e "\n${RED}Fail${WHITE} creating ssl keys"
+    docker cp certbot-${DOMAIN}:/var/log/letsencrypt/letsencrypt.log ./letsencrypt.log
+fi
+
+docker rm -f certbot-${DOMAIN}
+rm /etc/nginx/conf.d/0-certbot-${DOMAIN}.conf
+sudo nginx -s reload
